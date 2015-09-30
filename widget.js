469e1d449abf73c61f07ada3fedab4aa770cf69b
Joshua Carp <jm.carp@gmail.com>
2015-09-30 16:07:52 -0400

    Incomplete widget.

---
+++ widget.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/* global window, document */
+
+(function(window, $) {
+  function init(target) {
+    target = target || document.body;
+    var $form = $('<form>' +
+      '<textarea name="comment"></textarea>' +
+    '</form>').appendTo(target);
+    $form.on('submit', function(e) {
+      e.preventDefault();
+      sendFeedback({
+        comment: $form.find('[name=["comment"]').val()
+      });
+    });
+  }
+
+  function sendFeedback(data) {
+    $.ajax({
+      method: 'POST',
+      url: '',
+      data: $.extend({
+        url: window.location.url,
+        referer: document.referrer
+      }, data)
+    }).done(function(resp) {
+      console.log('submitted feedback');
+    }).fail(function() {
+      console.log('error submitting feedback');
+    });
+  }
+
+  return {
+    init: init,
+    sendFeedback: sendFeedback
+  };
+})(window, $);
