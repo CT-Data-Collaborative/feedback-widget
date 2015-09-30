ef406e34040ebb44243708ab513855b516006a64
Alexander Ose <alex@alexose.com>
2015-09-30 17:25:19 -0400

    building out widget.js

--- widget.js
+++ widget.js
@@ -3,32 +3,47 @@
 /* global window, document, require */
 
 var $ = require('jquery');
+  
+var template = '<form>'
+  +  '<label for="feedback-happy-button" >How do you feel?</label>'
+  +  '<button id="feedback-happy-button" value="happy">☺</button>'
+  +  '<button id="feedback-sad-button" value="unhappy">☹</button>'
+  +'</form>';
+  
+var css = {
+  position : 'fixed',
+  bottom : 0,
+  right: 0
+};
+
+var $form = $(template).css(css);
+
+$form.find('button').click(function(){
+  var value = $(this).val();
+  sendFeedback(value);
+});
 
 function init(target) {
+
   // script may be loaded from the <head>, so wait for the page to be fully parsed
   $(document).on('ready', function() {
     target = target || document.body;
     $form.appendTo(target);
   });
-  var $form = $('<form>' +
-    '<textarea name="comment"></textarea>' +
-  '</form>').appendTo(target);
-  $form.on('submit', function(e) {
-    e.preventDefault();
-    sendFeedback({
-      comment: $form.find('[name=["comment"]').val()
-    });
-  });
 }
 
-function sendFeedback(data) {
+function sendFeedback(upvote){
+
+  var data = {
+    url: window.location.url,
+    referer: document.referrer,
+    upvote: value
+  };
+
   $.ajax({
     method: 'POST',
     url: '',
-    data: $.extend({
-      url: window.location.url,
-      referer: document.referrer
-    }, data)
+    data: data 
   }).done(function(resp) {
     console.log('submitted feedback');
   }).fail(function() {
