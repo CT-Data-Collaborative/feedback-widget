b37ed1c1d24766b9539cdbce4220df94f38a8683
Joshua Carp <jm.carp@gmail.com>
2015-09-30 16:45:20 -0400

    Use webpack to compile widget script.

--- widget.js
+++ widget.js
@@ -1,44 +1,41 @@
 'use strict';
 
-/* global window, document */
+/* global window, document, require */
 
-window.FeedbackWidget = (function(window, $) {
-  function init(target) {
-    var $form = $('<form>' +
-      '<textarea name="comment"></textarea>' +
-    '</form>');
+var $ = require('jquery');
 
-    $form.on('submit', function(e) {
-      e.preventDefault();
-      sendFeedback({
-        comment: $form.find('[name=["comment"]').val()
-      });
+function init(target) {
+  // script may be loaded from the <head>, so wait for the page to be fully parsed
+  $(document).on('ready', function() {
+    target = target || document.body;
+    $form.appendTo(target);
+  });
+  var $form = $('<form>' +
+    '<textarea name="comment"></textarea>' +
+  '</form>').appendTo(target);
+  $form.on('submit', function(e) {
+    e.preventDefault();
+    sendFeedback({
+      comment: $form.find('[name=["comment"]').val()
     });
+  });
+}
 
-    // script may be loaded from the <head>, so wait for the page to be fully parsed
-    $(document).on('ready', function() {
-      target = target || document.body;
-      $form.appendTo(target);
-    });
-  }
+function sendFeedback(data) {
+  $.ajax({
+    method: 'POST',
+    url: '',
+    data: $.extend({
+      url: window.location.url,
+      referer: document.referrer
+    }, data)
+  }).done(function(resp) {
+    console.log('submitted feedback');
+  }).fail(function() {
+    console.log('error submitting feedback');
+  });
+}
 
-  function sendFeedback(data) {
-    $.ajax({
-      method: 'POST',
-      url: '',
-      data: $.extend({
-        url: window.location.url,
-        referer: document.referrer
-      }, data)
-    }).done(function(resp) {
-      console.log('submitted feedback');
-    }).fail(function() {
-      console.log('error submitting feedback');
-    });
-  }
-
-  return {
-    init: init,
-    sendFeedback: sendFeedback
-  };
-})(window, $);
+window.FeedbackWidget = {
+  init: init
+};
